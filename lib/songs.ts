9bcdc02dc05bbd9e78f0e4b465b3c500b8c6d15e
Axel Rojas <axelkevinagustinrojas@gmail.com>
2026-05-16 07:28:33 -0300

    baseUrl fix

--- lib/songs.ts
+++ lib/songs.ts
@@ -3,7 +3,9 @@
 export function rawUrl(path: string): string {
   const base =
     typeof window === 'undefined'
-      ? `http://localhost:${process.env.PORT ?? 3000}`
+      ? process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : `http://localhost:${process.env.PORT ?? 3000}`
       : ''
   return `${base}/${path}`
 }
